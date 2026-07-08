b7d551cd9aaf7e6900479211582dfbc338f2b74b
mounivelaga <mounivelaga@gmail.com>
2026-07-08 00:23:15 -0400

    loader.py code created

---
+++ etl/loader.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import sessionmaker
+
+from database.connection import engine
+from database.models import Order
+
+
+class DataLoader:
+    def __init__(self):
+        session = sessionmaker(bind=engine)
+        self.session = session()
+
+    def load(self, data):
+        for _, row in data.iterrows():
+            order = Order(
+                order_id=row["order_id"],
+                customer_name=row["customer_name"],
+                email=row["email"],
+                amount=row["amount"],
+                order_date=row["order_date"],
+            )
+            self.session.add(order)
+
+        self.session.commit()
+
+        print(f"{len(data)} Records loaded sucessfully.")
